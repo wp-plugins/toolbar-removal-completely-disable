3c9a11b31967a9119210e1ad4b0adce1487cdac6
sLaNGjI <sLaNGjI@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-25 02:08:33 +0000

    Cleaned HTML and Advertising
    git-svn-id: https://plugins.svn.wordpress.org/toolbar-removal-completely-disable/trunk@672794 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sLa2sLaNGjIs.php
+++ sLa2sLaNGjIs.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: √ sLa2sLaNGjI's
+Plugin Name: sLa2sLaNGjI's
 Plugin URI: http://slangji.wordpress.com/plugins/
-Description: &#9733;&#9733;&#9733; Author URL <a href="http://wordpress.org/extend/plugins/profile/sla" title="Author sLa">sLa</a> moved to <a href="http://wordpress.org/extend/plugins/profile/slangji" title="Author sLaNGjI">sLaNGjI</a> update bookmark! <code>Sample to Insert Informational Text on Header and Footer when Plugin is Active.</code> Work under <a href="http://www.gnu.org/licenses/gpl-2.0.html" title"GPLv2 or later License compatible">GPLv2</a> or later License. <a href="http://www.gnu.org/prep/standards/standards.html" title"GNU style indentation coding standard compatible">GNU style</a> indentation coding standard compatible. | <a href="http://slangji.wordpress.com/donate/" title="Free Donation">Donate</a> | <a href="http://slangji.wordpress.com/contact/" title="Send Me Bug and Suggestions">Contact</a> | <a href="http://profiles.wordpress.org/slangji" title="sLaNGjI's Profile @ WordPress.org">My Profile</a> | <a href="http://webscripts.softpedia.com/author/sLa-1869786722.html" title="sLa Developer Page @ SoftPedia.com">My SoftPedia</a> | <a href="http://slangji.wordpress.com/themes/" title="sLaNGjI's Custom Themes">My Themes</a> | <a href="http://wordpress.org/extend/plugins/wp-overview-lite/" title="Show Dashboard Overview and Footer Memory Load Usage">WP Overview?</a> | <a href="http://wordpress.org/extend/plugins/wp-missed-schedule/" title="Fix Missed Scheduled Future Posts Cron Job">WP Missed Schedule?</a> | <a href="http://wordpress.org/extend/plugins/wp-admin-bar-removal/" title="Remove Admin Bar Frontend Backend User Profile and Code">Admin Bar Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-admin-bar-node-removal/" title="Remove Admin Bar Frontend and Backend Node">Admin Bar Node Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-toolbar-removal/" title="Remove ToolBar Frontend Backend User Profile and Code">ToolBar Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-toolbar-node-removal/" title="Remove ToolBar Frontend and Backend Node">ToolBar Node Removal?</a> | <a href="http://wordpress.org/extend/plugins/wp-login-deindexing/" title="Total DeIndexing WordPress LogIn from all Search Engines">LogIn DeIndexing?</a> | <a href="http://wordpress.org/extend/plugins/wp-total-deindexing/" title="Total DeIndexing WordPress from all Search Engines">WP DeIndexing?</a> | <a href="http://wordpress.org/extend/plugins/wp-ie-enhancer-and-modernizer/" title="Enhancer and Modernizer IE Surfing Expirience">Enhancer IE Surfing?</a> | <a href="http://wordpress.org/extend/plugins/wp-wp-memory-db-indicator/" title="Memory Load Consumption db size Usage Indicator">Memory and db Indicator?</a>
+Description: Sample to Insert Informational Text Log on Header and Footer when Plugin is Activated.
 Version: 2009.1208.2009
 Author: sLa
 Author URI: http://slangji.wordpress.com/
@@ -13,53 +13,9 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Indentation: GNU style coding standard
 Indentation URI: http://www.gnu.org/prep/standards/standards.html
  *
- * STABLE Release: Version 2009 Build 1208 Revision 2009
+ * sLa2sLaNGjI's - Sample to Insert Informational Text Log on Header and Footer when Plugin is Activated.
  *
- * sLa2sLaNGjI's - WordPress PlugIn - Developmental Code in Becoming.
- *
- * √ CONTACT
- *
- * [Contact](http://slangji.wordpress.com/contact/)
- *
- * √ DONATIONS
- *
- * [Donate](http://slangji.wordpress.com/donate/)
- *
- * √ PROJECTS
- *
- * [My Plugins](http://slangji.wordpress.com/plugins/)
- * [My Themes](http://slangji.wordpress.com/themes/)
- *
- * √ DEVELOPER
- *
- * [phpBB.com](http://phpbb.com/)
- * [SoftPedia.com](http://webscripts.softpedia.com/author/sLa-1869786722.html)
- * [WordPress.org](http://profiles.wordpress.org/slangji)
- *
- * √ REPOSITORIES
- *
- * [GitHub.com](https://github.com/slangji)
- *
- * √ WEBSITE
- *
- * [WordPress.com](http://slangji.wordpress.com/)
- *
- * √ PROFILES
- *
- * [bbPress.org](http://bbpress.org/forums/profile/slangji/)
- * [BuddyPress.org](http://buddypress.org/community/members/slangji/profile/public/)
- * [Gravatar.com](http://en.gravatar.com/slangji)
- *
- * √ FOLLOW
- *
- * [LinkedIn](http://www.linkedin.com/in/slangjis)
- * [Twitter](https://twitter.com/sLanGjIs)
- * [FaceBook](https://www.facebook.com/sLaNGjI)
- * [Google+](https://plus.google.com/104369105810975562211)
- * [FeedBurner](http://feeds.feedburner.com/slangji)
- * [FriendFeed](http://friendfeed.com/slangjis)
- *
- * √ LICENSE
+ * Copyright (C) 2009-2013 [sLaNGjI's](http://slangji.wordpress.com/slangjis/) (email: <slangji[at]gmail[dot]com>)
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the [GNU General Public License](http://wordpress.org/about/gpl/)
@@ -72,8 +28,20 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * along with this program; if not, see [GNU General Public Licenses](http://www.gnu.org/licenses/),
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * √ THERMS
+ *
+ * This uses code derived from
+ * wp-footer-log.php by sLa <slangji[at]gmail[dot]com>
+ * sLa2sLaNGjIs.php by sLa <slangji[at]gmail[dot]com>
+ * according to the terms of the GNU General Public License version 2 (or later)
+ *
+ * According to the Terms of the GNU General Public License version 2 (or later) part of Copyright belongs to your own author and part belongs to their respective others authors:
+ *
+ * Copyright (C) 2009-2013 sLa (email: <slangji[at]gmail[dot]com>)
  *
  * √ DISCLAIMER
  *
@@ -88,38 +56,40 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
  *
  * The license for this software can be found on [Free Software Foundation](http://www.gnu.org/licenses/gpl-2.0.html) and as license.txt into this plugin package.
  *
+ * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
+ *
+ * √ VIOLATIONS
+ *
+ * [Violations of the GNU Licenses](http://www.gnu.org/licenses/gpl-violation.en.html)
+ * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
+ *
  * √ GUIDELINES
  *
- * This software meet detailed [Plugin Guidelines](http://wordpress.org/extend/plugins/about/guidelines/) paragraphs 1,4,10,12,13,16,17 quality requirements.
- * This readme.txt rocks. Seriously. Flying colors. It meet the specifications according to WordPress [Readme Validator](http://wordpress.org/extend/plugins/about/validator/) directives.
+ * This software meet [Detailed Plugin Guidelines](http://wordpress.org/extend/plugins/about/guidelines/) paragraphs 1,4,10,12,13,16,17 quality requirements.
  * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
  *
  * √ CODING
  *
  * This software implement [GNU style](http://www.gnu.org/prep/standards/standards.html) coding standard indentation.
+ * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
  *
- * √ COPYRIGHT
+ * √ VALIDATION
  *
- * Part of Copyright © 2009-2013 belongs to [sLaNGjI's](http://slangji.wordpress.com/slangjis/) (slangji[at]gmail[dot]com) and a portion to their respective owners.
+ * This readme.txt rocks. Seriously. Flying colors. It meet the specifications according to WordPress [Readme Validator](http://wordpress.org/extend/plugins/about/validator/) directives.
+ * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
  */
 /**
  * @package sLa2sLaNGjIs
  * @subpackage WordPress PlugIn
  * @since 2.1.0
  * @version 2009.1208.2009
- * @status STABLE Release
- * @development Code in Becoming
+ * @status STABLE (release)
  * @author sLa
- * @license GPLv2 or later
- * @indentation GNU style coding standard
- *
- * Sample to Insert Informational Text on Header and Footer when Plugin is Active.
- *
- * Work under GPLv2 or later License. GNU style indentation coding standard compatible.
+ * @license GPLv2 (or later)
+ * @indentation GNU (style coding standard)
  */
 	if (!function_exists('add_action'))
 		{
-			header('Status 403 Forbidden');
 			header('HTTP/1.0 403 Forbidden');
 			header('HTTP/1.1 403 Forbidden');
 			exit();
